0032b9de4691bd136e515101ab2401b3553a6e8d
abutler6588 <abutler6588@gmail.com>
2021-06-29 09:22:14 -0500

    refactored Test case for getPiResult

--- src/test/java/com/example/demo/MathControllerTest.java
+++ src/test/java/com/example/demo/MathControllerTest.java
@@ -1,19 +1,40 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(MathController.class)
 class MathControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+//    @Test
+//    public void getPiResult() {
+//        //SEAT
+//        //SETUP
+//        MathController mathController = new MathController();
+//        //EXEC
+//        String expected = "3.141592653589793";
+//        assertTrue(expected.equals(mathController.getPiMethod()));
+//    }
+
     @Test
-    public void getPiResult(){
-        //SEAT
-        //SETUP
-        MathController mathController = new MathController();
-        //EXEC
-        String expected = "3.141592653589793";
-        assertTrue(expected.equals(mathController.getPiMethod()));
+    public void getPiResult() throws Exception {
+
+        RequestBuilder request = get("/math/pi");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
     }
 
     @Test
